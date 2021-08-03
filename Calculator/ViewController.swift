8118b3e5deb8e8390d76b14b474eb945a82a12f7
Aleksandra Generowicz <a.generowicz@wp.pl>
2021-08-03 14:50:25 +0200

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Aleksandra Generowicz on 03/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func symbolButtonPressed(_ sender: UIButton) {
+        
+        
+    }
+    
+    @IBAction func numberButtonPressed(_ sender: UIButton) {
+        
+        
+    }
+    
+
+}
+
